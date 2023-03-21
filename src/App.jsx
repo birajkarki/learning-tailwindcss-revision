94634cdce352aa6421ddf1d4800767d94a83ee83
Biraj <birajkarki9849@gmail.com>
2023-03-21 23:52:10 +0545

    huy

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@
 
 
 import React from 'react'
-import BaseButton from './components/BaseButton'
 import LandingPage from './components/LandingPage'
 
 const App = () => {
   return (
     <>
     <LandingPage/>
-
-
-
     </>
 
   )
