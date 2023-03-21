9ac31392187c3bbc0a866c250affafa8ab47cc6a
Biraj <birajkarki9849@gmail.com>
2023-03-21 20:18:49 +0545

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,7 @@ const App = () => {
       <p class="text-lg text-black font-semibold">
         Erin Lindford
       </p>
-      <p class="text-slate-500 font-medium">
-        Product Engineer
-      </p>
+
     </div>
     <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
   </div>
